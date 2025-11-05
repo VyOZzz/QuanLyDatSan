3bca75ff03af7b5c4caad5d50a14031b055fd89d
Vy Pham <vyp8269@gmail.com>
2025-11-06 00:00:05 +0700

    update

--- src/main/java/com/codewithvy/quanlydatsan/dto/BookingRequest.java
+++ src/main/java/com/codewithvy/quanlydatsan/dto/BookingRequest.java
@@ -1,5 +1,6 @@
 package com.codewithvy.quanlydatsan.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -14,8 +15,10 @@ public class BookingRequest {
     private Long courtId; // ID của court cụ thể trong venues
 
     @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Ho_Chi_Minh")
     private LocalDateTime startTime;
 
     @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Ho_Chi_Minh")
     private LocalDateTime endTime;
 }
